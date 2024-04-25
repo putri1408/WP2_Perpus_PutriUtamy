883bc4acef45f3049d96bff43944c7547f97275a
putri1408 <putrinurutamy14@gmail.com>
2024-04-25 10:09:26 +0700

    menambahkan database

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+<h1><?php echo $judul ?></h1> 
+<h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Putri Nur </li> 
+        <li>Nama Belakang : Utamy</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan koramil tanah merah no.51</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Jakarta</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Bulutangkis</li> 
+        <li>senam</li> 
+    </ul> 
+</section>
